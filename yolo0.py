1384bdf21bc40b78f79268bfd2a44a0f7fb6acc3
Peter Dobrovoczki <peterdobrovoczki@gmail.com>
2019-04-13 11:36:59 +0200

    Modify config and weights to Tiny

--- yolo0.py
+++ yolo0.py
@@ -99,8 +99,8 @@ with open(classesFile, 'rt') as f:
     classes = f.read().rstrip('\n').split('\n')
 
 # Give the configuration and weight files for the model and load the network using them.
-modelConfiguration = "yolov3.cfg";
-modelWeights = "yolov3.weights";
+modelConfiguration = "yolov3-tiny.cfg";
+modelWeights = "yolov3-tiny.weights";
 
 net = cv.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
 net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
